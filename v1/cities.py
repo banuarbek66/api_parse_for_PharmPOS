9b85185383f8403c81ae8e91a4543ee1476c7384
Bakdaulet Anuarbekuly <bakda.anuar@mail.ru>
2025-12-24 17:23:01 +0500

    New Route Get list cities

--- v1/cities.py
+++ v1/cities.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 
 from deps import get_db
 from schemas import CityResponseCreate, CityResponseRead
 from repositories import CityResponseRepo
-
+from models import CityResponse
 router = APIRouter(prefix="/city-response", tags=["City Response"])
 
 
@@ -20,3 +20,13 @@ def create_city(data: CityResponseCreate, db: Session = Depends(get_db)):
     )
 
     return city
+
+
+@router.get("/list_normalized_cities", response_model=list[CityResponseRead])
+def get_cities(provider_name: str | None = Query(None), db: Session = Depends(get_db)):
+    if provider_name:
+        cities = db.query(CityResponse).filter(CityResponse.provider_name==provider_name).all()
+    else:
+        cities = db.query(CityResponse).all()
+    
+    return cities
